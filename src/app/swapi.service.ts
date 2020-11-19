19052bde2b4adc01e1ea97ce7ab9752560a5c378
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-11-19 11:40:52 -0600

    this is getting all 60 planets the right way

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -18,15 +18,21 @@ export class SwapiService {
     //   , tap(x => console.log(x))
     //   , repeat(10)
     // );
-    return merge(
-      this.httpSvc.get("https://swapi.dev/api/planets/")
-      , this.httpSvc.get("https://swapi.dev/api/planets/?page=2")
-      , this.httpSvc.get("https://swapi.dev/api/planets/?page=3")
-      , this.httpSvc.get("https://swapi.dev/api/planets/?page=4")
-      , this.httpSvc.get("https://swapi.dev/api/planets/?page=5")
-      , this.httpSvc.get("https://swapi.dev/api/planets/?page=6")
-    ).pipe(
-      map(x => (x as any).results.map(y => ({name: y.name})))
+
+    // return merge(
+    //   this.httpSvc.get("https://swapi.dev/api/planets/")
+    //   , this.httpSvc.get("https://swapi.dev/api/planets/?page=2")
+    //   , this.httpSvc.get("https://swapi.dev/api/planets/?page=3")
+    //   , this.httpSvc.get("https://swapi.dev/api/planets/?page=4")
+    //   , this.httpSvc.get("https://swapi.dev/api/planets/?page=5")
+    //   , this.httpSvc.get("https://swapi.dev/api/planets/?page=6")
+    // ).pipe(
+    //   map(x => (x as any).results.map(y => ({name: y.name})))
+    // );
+
+    return this.httpSvc.get("https://swapi.dev/api/planets/").pipe(
+      expand(x => (x as any).next ? this.httpSvc.get((x as any).next.replaceAll("http: , https:")) : EMPTY)
+      ,map(x => (x as any).results.map(y => ({name: y.name})))
     );
   }
 }
