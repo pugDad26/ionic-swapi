2105fe23a31a177c7cd95f12691a519df56dc171
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-11-27 15:34:06 -0600

    useHash a trick to make ionic apps behave better in github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,13 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(
+      routes
+      , { 
+        preloadingStrategy: PreloadAllModules
+        , useHash: true 
+      }
+    )
   ],
   exports: [RouterModule]
 })
