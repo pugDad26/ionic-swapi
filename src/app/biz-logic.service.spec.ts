3e0841762c62d81feb1af9669ed174a97fac343b
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-11-27 15:26:55 -0600

    wrote two new unit tests
    intro to unit tests

--- src/app/biz-logic.service.spec.ts
+++ src/app/biz-logic.service.spec.ts
@@ -13,4 +13,18 @@ describe('BizLogicService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(
+    'getDisplayColor() should color Tatoine green'
+    , () => {
+      expect(
+        service.getDisplayColor({name: 'Tatooine'})
+      ).toBe('green')
+    }
+  );
+
+  it(
+    'getDisplayColor() should color unknown goldenrod'
+    , () => expect(service.getDisplayColor({name: 'unknown'})).toBe('goldenrod')
+  );
 });
